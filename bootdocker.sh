f5c767e282a4e71c5cbee9b9359218054811dbd4
Ian hunt <ihuntenator@gmail.com>
2021-06-15 16:07:08 +1200

    bash to install latest docker-compose

--- bootdocker.sh
+++ bootdocker.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
-# Docker CE
 yum -y update
 yum-config-manager --enable extras
-yum install -y yum-utils device-mapper-persistent-data lvm2
-yum-config-manager  --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+yum install -y yum-utils device-mapper-persistent-data lvm2 curl
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 yum install docker-ce docker-ce-cli containerd.io
 systemctl enable docker
 systemctl start docker
 systemctl status docker
 
-# Docker-compose
-yum install curl
-curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+DCVER=$(curl --silent "https://github.com/docker/compose/releases/latest")
+VER=$(grep -Po '(?<=\/tag\/)(\d+.\d+.\d+)' <<<$DCVER)
+curl -kLs "https://github.com/docker/compose/releases/download/$VER/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose && chmod 755 /usr/local/bin/docker-compose
